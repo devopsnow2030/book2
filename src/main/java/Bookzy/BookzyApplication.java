632eaaf418c97a0ddd030ce1efb650248d3214b5
Haytham Osman <osman.online@yahoo.com>
2022-03-02 20:48:15 -0800

    Welcome Haytham

---
+++ src/main/java/Bookzy/BookzyApplication.java
@@ -0,0 +1,24 @@
+package Bookzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@SpringBootApplication
+public class BookzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyApplication.class, args);
+	}
+
+
+	@RequestMapping("/")
+	public String index() {
+		return "rrtrt trtrt rtr trt rt trrt ";
+	}
+
+
+}
